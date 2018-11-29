f9078d5c55eedf646747623d7e7e0b7b99c95312
najdi123 <ali.najdi.1989@gmail.com>
2018-11-30 00:07:30 +0330

    moved the container in about

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,20 +13,22 @@ import FooterPage from "../components/Footer";
 class Home extends Component {
     render() {
         return (
-            <div className="about container">
+            <div className="about">
 
                 {/*<SideNavPage/>*/}
                 {/*<NavbarPage/>*/}
                 {/*<SideNavPage/>*/}
                 {/*<CarouselPage/>*/}
                 <Navbar/>
-                <h4 className="desc5">
-                    هدف ما تفکیک پسماند قابل بازیافت، معرفی همکاران به یکدیگر، حذف واسطه و اشتغال زایی در راستای حفظ محیط زیست می باشد
-                </h4>
-                <br/>
-                <h4 className="desc5">
-                    بنگاه ضایعاتی ابراهیمی با بیش از بیست سال تجربه آماده به خدمت رسانی می باشد
-                </h4>
+                <div className="container">
+                    <h4 className="desc5">
+                        هدف ما تفکیک پسماند قابل بازیافت، معرفی همکاران به یکدیگر، حذف واسطه و اشتغال زایی در راستای حفظ محیط زیست می باشد
+                    </h4>
+                    <br/>
+                    <h4 className="desc5">
+                        بنگاه ضایعاتی ابراهیمی با بیش از بیست سال تجربه آماده به خدمت رسانی می باشد
+                    </h4>
+                </div>
 
                 <FooterPage/>
             </div>
